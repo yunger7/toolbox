e2f92a1be701196d987de2cb916c8b5446b06720
yunger <luisgalete6264@gmail.com>
2022-07-21 13:51:49 -0300

    chore(*): setup mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,57 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useState } from "react";
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
+import { useHotkeys } from "@mantine/hooks";
+import { getCookie, setCookies } from "cookies-next";
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+import type { GetServerSidePropsContext } from "next";
+import type { AppProps } from "next/app";
+import type { ColorScheme } from "@mantine/core";
+
+export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+	const { Component, pageProps } = props;
+
+	const [colorScheme, setColorScheme] = useState<ColorScheme>(
+		props.colorScheme
+	);
+
+	function toggleColorScheme(value?: ColorScheme) {
+		const nextColorScheme =
+			value || (colorScheme === "dark" ? "light" : "dark");
+		setColorScheme(nextColorScheme);
+		setCookies("yungerdev-color-scheme", nextColorScheme, {
+			maxAge: 60 * 60 * 24 * 30,
+		});
+	}
+
+	useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
+	return (
+		<>
+			<Head>
+				<title>Toolbox | yunger.dev</title>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+
+			<ColorSchemeProvider
+				colorScheme={colorScheme}
+				toggleColorScheme={toggleColorScheme}
+			>
+				<MantineProvider
+					withGlobalStyles
+					withNormalizeCSS
+					theme={{ colorScheme }}
+				>
+					<Component {...pageProps} />
+				</MantineProvider>
+			</ColorSchemeProvider>
+		</>
+	);
 }
 
-export default MyApp;
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+	colorScheme: getCookie("yungerdev-color-scheme", ctx) || "light",
+});
